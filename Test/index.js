a94d02d535a2b0011e5d3af949fc1ac9702046d2
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-15 19:58:45 +0100

    Update index.js

--- Test/index.js
+++ Test/index.js
@@ -115,7 +115,7 @@ const activities_list = [
         setInterval(() => {
             const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
             client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-        }, 7000); // Runs this every 10 seconds.
+        }, 5000); // Runs this every 10 seconds.
     
 });
 
