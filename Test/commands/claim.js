d83588833ab4f42204eb6b60e29ba2fe3e9cc79e
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-15 09:33:54 +0100

    Update claim.js

--- Test/commands/claim.js
+++ Test/commands/claim.js
@@ -1,26 +1,24 @@
 const discord = require("discord.js");
 
 module.exports.run = async (client, message, argument) => {
-    if (!message.member.hasPermission("KICK_MEMBER")) return message.reply("Jij kan dit niet doen");   
+
+  if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("sorry jij kan dit niet");  
      
 
     var botEmbed = new discord.MessageEmbed()
+   
+    
     .setTitle('Claim')
-    .setDescription("Je hebt deze ticket geclaimt")
-
-    .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
-    .setImage('')
+    .setColor("#f00c0c")
     .setTimestamp()
-    .setFooter('Defensie', '');
-
-
+    .setDescription("TEST")
+    
+   
     var channel = message.member.guild.channels.cache.get("761879667124600842");
 
     if (!channel) return
-    
 
     channel.send(botEmbed);
-
   
 }
 
@@ -29,3 +27,4 @@ module.exports.help = {
     description: "Geeft al de verschillende commands",
     category: "Informatie"
 }
+
