d49a86eabeb99e950deb25088194d0153a9769ab
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-15 15:18:16 +0100

    Update index.js

--- Test/index.js
+++ Test/index.js
@@ -104,12 +104,19 @@ client.on("guildMemberRemove", member => {
 });
 
 
-client.on("ready", async () => {
-
-    console.log(`${client.user.username} is online.`);
-
-    client.user.setActivity("Kazerne Noord-Holland V2 {coming soon}", { type: "PLAYING" });
-
+const activities_list = [
+    "with the &help command.", 
+    "with the developers console",
+    "with some code", 
+    "with JavaScript"
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
+    
+    client.on('ready', () => {
+        setInterval(() => {
+            const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+            client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+        }, 10000); // Runs this every 10 seconds.
+    
 });
 
 client.on("messageDelete", messageDeleted => {
