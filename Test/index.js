255d2db4d31fb94118a3cd5364acc8262817771b
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-15 15:06:38 +0100

    Update index.js

--- Test/index.js
+++ Test/index.js
@@ -104,7 +104,7 @@ client.on("guildMemberRemove", member => {
 });
 
 
-var activevar = ["with the &help command.", "with the developers console", "with some code", "with JavaScript"];
+var activevar = ["Karzerne Noord-Holland", "with the developers console", "with some code", "with JavaScript"];
 var activities = activevar[Math.floor(Math.random()*activevar.length)];
 client.on('ready', () => {
     client.user.setActivity(activities);
