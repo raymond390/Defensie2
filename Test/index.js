cef5387665d465a9874c23a74e7e36127121de2d
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-16 12:54:54 +0100

    Update index.js

--- Test/index.js
+++ Test/index.js
@@ -108,14 +108,14 @@ const activities_list = [
     "Kazerne Noord-Holland", 
     ".help",
     "Kazerne Noord-Holland", 
-    "in ontwikeling"
+    ".help"
     ]; // creates an arraylist containing phrases you want your bot to switch through.
     
     client.on('ready', () => {
         setInterval(() => {
             const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
             client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-        }, 5000); // Runs this every 10 seconds.
+        }, 2500); // Runs this every 10 seconds.
     
 });
 
