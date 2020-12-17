6b7631ae6f26c150ef0bcac1eac3c0cf56a08820
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-17 13:39:25 +0100

    Update announcement.js

--- Test/commands/announcement.js
+++ Test/commands/announcement.js
@@ -36,7 +36,7 @@ module.exports.run = async (client, message, args) => {
     var announceEmbed = new discord.MessageEmbed()
         .setTitle(`${options.titel}`)
         .setColor(options.kleur)
-        .setDescription(`Bericht van ${message.author} \n ${options.bericht} \n `)
+        .setDescription(` ${options.bericht} \n Bericht van ${message.author} \n  `)
         .setTimestamp();
 
     var channel = message.member.guild.channels.cache.find(channels => channels.name === options.kanaal);
