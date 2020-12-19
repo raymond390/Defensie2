bafa267b07e27287bc1481f3f1aeced2d9161e9f
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-19 16:13:21 +0100

    Update dildo.js

--- Test/commands/dildo.js
+++ Test/commands/dildo.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     .setDescription("een dilo voor Jamy")
     .setColor("#521441")
     .setThumbnail('https://yt3.ggpht.com/a/AGF-l7_P42jUnWAoIvDDHzQH27EIOs5hAoDAwfdgug=s900-c-k-c0xffffffff-no-rj-mo')
-    .setImage('https://hwimages.beslist.net/beslist-images/32BKzpy9LUAzWYemm1pfj2SYq15G/394/V800/cdc232f2f463edda11b787682c4affa1/Realistische-dildo%27s/Extreme-Oversized-Xxxxxxl-Dildo.jpg')
+    .setImage(message.guild.iconURL())
     .setTimestamp()
     .setFooter('Defensie', '');
 
@@ -19,7 +19,7 @@ return message.channel.send(botEmbed);
 }
 
 module.exports.help = {
-    name: "dildo",
+    name: "plaatje",
     description: "Geeft al de verschillende commands",
     category: "Informatie"
 }
