1fe2577b189e17e3d72db7a94e1871a00fcd60f7
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-30 12:12:40 +0100

    Update uitslag.js

--- Test/commands/uitslag.js
+++ Test/commands/uitslag.js
@@ -102,5 +102,5 @@ module.exports.run = async (client, message, args) => {
 module.exports.help = {
     name: "uitslag",
     description: "Hier mee geef je de uitslag van een sollicitaite",
-    category: "Algemeen"
+    category: "Informatie"
 }
